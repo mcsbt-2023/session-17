aa633b67c542796e568c2d56b5f64083a1be921e
Pepe García <pepe@pepegar.com>
2023-11-27 10:24:38 +0100

    add code from class

--- exceptions.py
+++ exceptions.py
@@ -10,5 +10,35 @@ except:
     print('the file doesn\'t exist')
 
 
+# %%
+
+def say_hello_to_student():
+    name = input("tell me your name: ")
+
+    if name == "Pepe":
+        raise ValueError("name should be a student's name, Pepe")
+
+    print("hello " + name)
+
+try:
+    say_hello_to_student()
+except:
+    print("try again with a student's name")
+
+#%%
+def func3():
+    open("potato.txt")
+
+def func2():
+    func3()
+
+def func1():
+    try:
+        func2()
+    except:
+        print("something failed")
+
+func1()
+print("Hey Paula")
 
 # %%
